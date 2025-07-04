cd171c637f84fd17073fed53ac4892768a383594
Jakub Beránek <berykubik@gmail.com>
2025-07-04 11:25:10 +0200

    Stream output of certain commands to the terminal
    To provide colourful output and more context for the user.

--- src/utils.rs
+++ src/utils.rs
@@ -6,9 +6,23 @@ pub fn run_command<'a, Args: AsRef<[&'a str]>>(args: Args) -> anyhow::Result<Str
     run_command_at(args, &std::env::current_dir()?)
 }
 
+/// Run command while streaming stdout and stderr to the terminal.
+pub fn stream_command<'a, Args: AsRef<[&'a str]>>(args: Args) -> anyhow::Result<()> {
+    run_command_inner(args, &std::env::current_dir()?, false)?;
+    Ok(())
+}
+
 pub fn run_command_at<'a, Args: AsRef<[&'a str]>>(
     args: Args,
     workdir: &Path,
+) -> anyhow::Result<String> {
+    run_command_inner(args, workdir, true)
+}
+
+fn run_command_inner<'a, Args: AsRef<[&'a str]>>(
+    args: Args,
+    workdir: &Path,
+    capture: bool,
 ) -> anyhow::Result<String> {
     let args = args.as_ref();
 
@@ -17,16 +31,32 @@ pub fn run_command_at<'a, Args: AsRef<[&'a str]>>(
     cmd.args(&args[1..]);
 
     eprintln!("+ {cmd:?}");
-    let out = cmd.output().expect("command failed");
-    let stdout = String::from_utf8_lossy(out.stdout.trim_ascii()).to_string();
-    let stderr = String::from_utf8_lossy(out.stderr.trim_ascii()).to_string();
-    if !out.status.success() {
-        Err(anyhow::anyhow!(
-            "Command `{cmd:?}` failed with exit code {:?}. STDOUT:\n{stdout}\nSTDERR:\n{stderr}",
-            out.status.code()
-        ))
+    if capture {
+        let out = cmd.output().expect("command failed");
+        let stdout = String::from_utf8_lossy(out.stdout.trim_ascii()).to_string();
+        let stderr = String::from_utf8_lossy(out.stderr.trim_ascii()).to_string();
+        if !out.status.success() {
+            Err(anyhow::anyhow!(
+                "Command `{cmd:?}` failed with exit code {:?}. STDOUT:\n{stdout}\nSTDERR:\n{stderr}",
+                out.status.code()
+            ))
+        } else {
+            Ok(stdout)
+        }
     } else {
-        Ok(stdout)
+        let status = cmd
+            .spawn()
+            .expect("cannot spawn command")
+            .wait()
+            .expect("command failed");
+        if !status.success() {
+            Err(anyhow::anyhow!(
+                "Command `{cmd:?}` failed with exit code {:?}",
+                status.code()
+            ))
+        } else {
+            Ok(String::new())
+        }
     }
 }
 
